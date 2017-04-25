2a62378b4717681451b7b20f1fbff2c41c67e91c
Cyborg_Ean <ean.director@gmail.com>
2017-04-25 14:52:42 -0700

    updating architecture

--- client/js/clientSend.js
+++ client/js/clientSend.js
@@ -3,6 +3,12 @@
 Client.movePlayer = function(axis,speed){
     var data = {};
     data.axis = axis;
-    data.speed = speed;
+    data.force = speed;
     Client.socket.emit('move_player',data);
+}
+
+Client.changeColor = function(color){
+    var data = {};
+    data.tint = color;
+    Client.socket.emit('changeGraphics',data);
 }
