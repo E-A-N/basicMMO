21e800096739d6cb1be72450ca7944df51accd1c
Cyborg_Ean <ean.director@gmail.com>
2017-04-25 15:33:08 -0700

    working on automatically allocating packets

--- client/js/clientSend.js
+++ client/js/clientSend.js
@@ -12,11 +12,26 @@ Client.changeColor = function(color){
     data.tint = color;
     Client.socket.emit('changeGraphics',data);
 }
-Client.sendToServer = function(message,data){
+
+Client.sendToServer = function(message, data){
+    var allocatedData = {};
     switch(message){
         case 'move_player'
 
 
 
+
+
+    }
+}
+
+function allocate(data){
+    var allocatedData = {};
+    var dataList = Object.keys(data);
+    var allData = dataList.legnth;
+    var member;
+    for(var i = 0; i < allData; i++){
+        member = dataList[i] + '';
+        allocatedData[member] = data.dataList[i];
     }
 }
