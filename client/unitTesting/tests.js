538b7d1db0b2db4733cbfce58e46dd62cb8d9b64
Cyborg_Ean <ean.director@gmail.com>
2017-05-03 13:37:31 -0700

    Implemented current test for network events attributes

--- client/unitTesting/tests.js
+++ client/unitTesting/tests.js
@@ -2,7 +2,7 @@ QUnit.test( "Client Attributes Test", testClientAttributes);
 
 function testClientAttributes(assert){
     //Define pass/fail message
-    var goodTest = " has passed!";
+    var goodTest = "Passed!";
 
     //Create and initialize test client object
     var _tc = new NetEvents;
@@ -10,10 +10,12 @@ function testClientAttributes(assert){
     var game = {}; //for testing only this value will replace phaser instance
     _tc.init(ip,game);
 
-    assert.ok(_tc.movePlayer === "movePlayer",_tc.movePlayer + goodTest);
+    assert.ok(_tc.movePlayer === "movePlayer", goodTest);
     assert.ok(_tc.changeGraphics === "changeGraphics", goodTest);
     assert.ok(_tc.update === "stateUpdate", goodTest);
     assert.ok(_tc.disconnect === "disconnect", goodTest);
     assert.ok(_tc.removePlayer === "removePlayer", goodTest);
-
+    assert.ok(_tc.addNewPlayer === "addNewPlayer", goodTest);
+    assert.ok(_tc.addPlayers === "addPlayers", goodTest);
+    assert.ok(_tc.joinedGame === "joinGameSuccess", goodTest);
 }
