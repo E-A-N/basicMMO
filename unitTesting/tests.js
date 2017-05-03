6f0076909962954742a7b9697d66bcab919b0222
Cyborg_Ean <ean.director@gmail.com>
2017-05-03 14:44:20 -0700

    Resolving errors in testing suite

--- unitTesting/tests.js
+++ unitTesting/tests.js
@@ -1,15 +1,26 @@
-QUnit.test( "Client Attributes Test", testClientAttributes);
+QUnit.test( "Client Attributes Test", testClientInstanceAttributes);
 
-function testClientAttributes(assert){
+
+//Create and initialize test client object
+var _tc = new NetEvents;
+var ip = "http://127.0.0.1:7777";
+
+//Create phaser game instance for testing
+var testID = "testCanvas"
+var testGame = new Phaser.Game(800,450,Phaser.AUTO, testID);
+_tc.init(ip,testGame);
+
+var hideGame = document.getElementById(testID);
+hideGame.style.display = 'none';
+
+function testClientInstanceAttributes(assert){
     //Define pass/fail message
     var goodTest = "Passed!";
 
-    //Create and initialize test client object
-    var _tc = new NetEvents;
-    var ip = "http://127.0.0.1:7777";
-    var game = {}; //for testing only this value represents phaser instance
-    _tc.init(ip,game);
+    //Game Instance
+    assert.ok(_tc.game !== null, "Phaser instance has passed!");
 
+    //Server Message Attributes
     assert.ok(_tc.movePlayer === "movePlayer", goodTest);
     assert.ok(_tc.changeGraphics === "changeGraphics", goodTest);
     assert.ok(_tc.update === "stateUpdate", goodTest);
@@ -18,4 +29,14 @@ function testClientAttributes(assert){
     assert.ok(_tc.addNewPlayer === "addNewPlayer", goodTest);
     assert.ok(_tc.addPlayers === "addPlayers", goodTest);
     assert.ok(_tc.joinedGame === "joinGameSuccess", goodTest);
+
+}
+
+
+function testClientPlayerAllocationMethod(assert){
+
+    var goodTest = "Passed";
+
+
+
 }
